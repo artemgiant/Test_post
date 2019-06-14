701cb9cc0a3532c0b30d8e9014872c7787201ce6
Grosesdorf <grosesdorf@outlook.com>
2019-06-14 11:35:21 +0300

    fixed edit profile

--- src/Controller/ProfileController.php
+++ src/Controller/ProfileController.php
@@ -3,11 +3,9 @@
 namespace App\Controller;
 
 use App\Form\ProfileFormType;
-use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\HttpFoundation\File\Exception\FileException;
 use Symfony\Component\HttpFoundation\File\UploadedFile;
 
 /**
@@ -36,19 +34,22 @@ class ProfileController extends CabinetController
              * @var UploadedFile $file
              */
             $file = $form->get('avatarFile')->getData();
-            $fileName = $this->generateUniqueFileName().'.'.$file->guessExtension();
-            $file->move($this->getParameter('avatar_directory'), $fileName);
-            //kostil
-            $this->user->setAvatar('sklad-express/uploads/avatars/'.$fileName);
+            if($file)
+            {
+                $fileName = $this->generateUniqueFileName().'.'.$file->guessExtension();
+                $file->move($this->getParameter('avatar_directory'), $fileName);
+                //kostil
+                $this->user->setAvatar('sklad-express/uploads/avatars/'.$fileName);
+            }
+
             $entityManager = $this->getDoctrine()->getManager();
             $entityManager->persist($this->user);
             $entityManager->flush();
-            $mess='Save sucess';
-           // return $this->redirectToRoute('post_profile');
+            $mess='Save success';
         }
-        elseif($form->isSubmitted() && !$form->isValid()){
+        elseif($form->isSubmitted() && !$form->isValid())
+        {
             $errors=(string)$form->getErrors(true, false);
-
         }
 
         return $this->render('cabinet/profile/profile.html.twig', [
@@ -59,48 +60,6 @@ class ProfileController extends CabinetController
             'my_address' => $this->getMyAddress($this->user->getId()),
             'page_id'=>'post_profile'
         ]);
-    }
-
-    /**
-     * @Route("/edit", name="post_profile_edit")
-     * @param Request $request
-     * @return RedirectResponse
-     */
-    public function profileEditAction(Request $request)
-    {
-        $this->user = $this->getUser();
-
-        $form = $this->createForm(ProfileFormType::class, $this->user);
-
-        $form->handleRequest($request);
-
-        $errors =[];
-
-        if ($form->isSubmitted() && $form->isValid())
-        {
-            /**
-             * @var UploadedFile $file
-             */
-            $file = $form->get('avatar')->getData();
-
-            $fileName = $this->generateUniqueFileName().'.'.$file->guessExtension();
-            $file->move($this->getParameter('avatar_directory'), $fileName);
-            //kostil
-            $this->user->setAvatar('sklad-express/uploads/avatars/'.$fileName);
-            $entityManager = $this->getDoctrine()->getManager();
-            $entityManager->persist($this->user);
-            $entityManager->flush();
-
-            return $this->redirectToRoute('post_profile');
-        }
-        elseif ($form->isSubmitted() && !$form->isValid())
-        {
-            $errors = $form->getErrors(true);
-
-            return $this->redirectToRoute('post_profile',[
-                'errors' => $errors,
-            ]);
-        }
     }
 
     /**
