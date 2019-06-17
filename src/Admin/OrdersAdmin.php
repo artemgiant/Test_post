1daddd3b0334f4c5ad494449b12897ecaa3c0825
Grosesdorf <grosesdorf@outlook.com>
2019-06-17 18:16:06 +0300

    added orders(parcels) admin.. translate..

---
+++ src/Admin/OrdersAdmin.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace App\Admin;
+
+use App\Entity\OrderStatus;
+use App\Entity\User;
+use Sonata\AdminBundle\Datagrid\DatagridMapper;
+use Sonata\AdminBundle\Datagrid\ListMapper;
+use Sonata\AdminBundle\Form\FormMapper;
+use Sonata\AdminBundle\Admin\AbstractAdmin;
+
+use Sonata\AdminBundle\Form\Type\ModelType;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+
+class OrdersAdmin extends AbstractAdmin
+{
+    protected $baseRoutePattern = 'orders_status';
+    protected $baseRouteName = 'orders_status';
+    /**
+     * @param DatagridMapper $datagridMapper
+     */
+    protected function configureDatagridFilters(DatagridMapper $datagridMapper): void
+    {
+        $datagridMapper->add('orderStatus');
+    }
+
+    /**
+     * @param ListMapper $listMapper
+     */
+    protected function configureListFields(ListMapper $listMapper): void
+    {
+        $listMapper->addIdentifier('id')
+            ->add('user', null, [], EntityType::class, [
+                'class' => User::class,
+                'choice_label' => 'name',
+            ])
+            ->add('order_status', null, [], EntityType::class, [
+                'class' => OrderStatus::class,
+                'choice_label' => 'order_status',
+            ])
+            ->addIdentifier('sendFromAddress')
+            ->addIdentifier('comment');
+    }
+
+    /**
+     * @param FormMapper $formMapper
+     */
+    protected function configureFormFields(FormMapper $formMapper): void
+    {
+        $userFieldOptions = [];
+        $orderStatusFieldOptions = [];
+        $addressesFieldOptions = [];
+
+        $formMapper
+            ->add('user', ModelType::class, $userFieldOptions)
+            ->add('orderStatus', ModelType::class, $orderStatusFieldOptions)
+            ->add('addresses', ModelType::class, $addressesFieldOptions)
+            ->add('trackingNumber')
+            ->add('volumeWeigth')
+            ->add('declareValue')
+            ->add('sendFromAddress')
+            ->add('sendFromIndex')
+            ->add('sendFromCity')
+            ->add('sendFromPhone')
+            ->add('sendFromEmail')
+            ->add('sendDetailPlaces')
+            ->add('sendDetailWeight')
+            ->add('sendDetailLength')
+            ->add('sendDetailWidth')
+            ->add('sendDetailHeight')
+            ->add('comment')
+            ->add('email')
+            ->add('address')
+            ->add('shippingCosts')
+            ->add('deliveryStatus')
+            ->add('country')
+            ->add('fromCountry')
+            ->add('city')
+            ->add('zip')
+            ->add('towarehouse')
+            ->add('quantity');
+    }
+}
