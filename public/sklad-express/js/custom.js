6e3bf6ae7e52191abf72934fb972b09c738da39d
Artem <you@example.com>
2019-08-05 11:04:21 +0300

    VIP User,Login Form FLAG,validation Form register

--- public/sklad-express/js/custom.js
+++ public/sklad-express/js/custom.js
@@ -46,7 +46,7 @@ $(document).ready(function() {
 
 $('.selectpicker').selectpicker();
 
-   $('form').on('keyup','input:not(#exampleInputEmail1,#exampleInputPassword1),textarea:not(#support_Message,#order_form_comment,#order_form_products_0_descUa)',function () {
+   $('form').on('keyup','input:not(#exampleInputPassword1),textarea:not(#support_Message,#order_form_comment,#order_form_products_0_descUa)',function () {
         var pattern = new RegExp('^([a-zA-Z0-9\\.\\,\\@]+)$'),
             el =$(this).closest('.form-group');
         if(!pattern.test( $(this).val())){
