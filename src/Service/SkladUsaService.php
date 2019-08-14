27015c594c69fc68e5b77a08aca183f10b239bba
Valera <valera@local.com>
2019-08-14 10:52:25 +0300

    Add api service for SkladUsa

---
+++ src/Service/SkladUsaService.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace App\Service;
+
+use App\Entity\Order;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\ORM\EntityManager;
+
+class SkladUsaService
+{
+
+    protected $api_base_url = 'http://localhost:8080';
+    protected $path_econom = '/api/order_expressposhta_econom/';
+    protected $path_express = '/api/order_expressposhta_express/';
+    protected $api_key = 'MjpKV3JzVHFtNFlDcFhNdmNQRnhNVXVITlR6cmoxTUw='; // w
+//    protected $api_key = 'NTcxOm1IWkFuZzN3SVk0UXlGMjhDSG91OVU4dlFFSWlWSw=='; // EP
+//    protected $api_key = 'MjpMTFF0eHF3OUFubU4yUWJvc3hsbldHRko1MHhXWkk'; // h
+
+    // Send Orders data from Expressposhta to Sklad
+
+    public function sendOrderToSklad(Order $order){
+
+        $data = new ArrayCollection();
+
+        $data->epOrderId = $order->getId();
+        $data->receiverName = $order->getAddresses()->getFullName();
+        $data->receiverEmail = $order->getUser()->getEmail();
+        $data->receiverAddress = $order->getAddresses()->getAddress();
+        $data->receiverCity = $order->getAddresses()->getCity();
+        $data->receiverState = $order->getAddresses()->getRegionOblast();
+        $data->receiverZip = $order->getAddresses()->getZip();
+        $data->receiverCountry = $order->getAddresses()->getCountry()->getShortName();
+        $data->trackingNumber = $order->getTrackingNumber();
+        $data->shippingCompanyToUsa = $order->getCompanySendToUsa();
+        $data->trackingNumberToUsa = $order->getSystemNum();
+        $data->shippingCompanyInUsa = $order->getCompanySendInUsa();
+        $data->trackingNumberInUsa = $order->getSystemNumInUsa();
+        $data->comment = $order->getComment();
+        $data->productsData = [];
+        foreach ($order->getProducts() as $product) {
+            $data->productsData[]  = [
+                'descrEn' => $product->getDescEn(),
+                'descrUa' => $product->getDescUa(),
+                'count' => $product->getCount(),
+                'price' => $product->getPrice(),
+            ];
+        }
+
+        $data_json = json_encode($data);
+        $headers = array(
+            'Authorization: ' . $this->api_key,
+            'Content-Type: application/json',
+            'Content-Length: ' . strlen($data_json)
+        );
+        $requestUrl = '';
+        if($order->getOrderType()->getCode() == 'econom'){
+            $requestUrl = $this->api_base_url.$this->path_econom;
+        }
+        if($order->getOrderType()->getCode() == 'express'){
+            $requestUrl = $this->api_base_url.$this->path_express;
+        }
+
+        $curlObj = curl_init();
+        curl_setopt($curlObj, CURLOPT_URL,$requestUrl);
+        curl_setopt($curlObj, CURLOPT_HTTPHEADER, $headers);
+        curl_setopt($curlObj, CURLOPT_CUSTOMREQUEST, 'PUT');
+        curl_setopt($curlObj, CURLOPT_POSTFIELDS, $data_json);
+        curl_setopt($curlObj, CURLOPT_RETURNTRANSFER, true);
+
+        $response  = curl_exec($curlObj);
+        curl_close($curlObj);
+        unset($curlObj);
+
+        return $response;
+
+    }
+
+}
