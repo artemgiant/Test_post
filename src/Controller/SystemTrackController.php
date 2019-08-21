c5cb05739ed2b42b19eb1cb878b2c736049e74e5
proger <bonderolll@gmail.com>
2019-08-21 16:22:18 +0300

    fix send

--- src/Controller/SystemTrackController.php
+++ src/Controller/SystemTrackController.php
@@ -3,6 +3,8 @@
 namespace App\Controller;
 
 use App\Entity\Order;
+use App\Entity\OrderStatus;
+use App\Admin\OrdersAdmin;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
@@ -15,14 +17,22 @@ class SystemTrackController extends AbstractController
      */
     public function OutsideAction(Request $request)
     {
-
+        $entityManager = $this->getDoctrine()->getManager();
         if($request->getMethod()== "POST" && (($request->request->get('_token'))!= "BWufe5fBhHD7dqI6Nds3YKOXBHwNuEjj0Kwm")){
             return $this->render("bundles/TwigBundle/Exception/error404.html.twig");
         }
         $OrderReposotory =  $this->getDoctrine()->getRepository(Order::class);
+        /** @var Order $Order */
         $Order =  $OrderReposotory->find($request->request->get('id_order'));
+
+
         $Order->setSystemNum($request->request->get('systemNum'));
-        $entityManager = $this->getDoctrine()->getManager();
+        $orderStatus = $entityManager->getRepository(OrderStatus::class)->findOneBy(['status' => 'complit']);
+        $Order->setOrderStatus($orderStatus);
+            $companyTmp=$request->request->get('company',false);
+
+            $company = ($companyTmp && in_array(OrdersAdmin::CARRIER_CODES))?$companyTmp:'usps';
+            $Order->setCompanySendToUsa($company);
         $entityManager->persist($Order);
         $entityManager->flush();
 
@@ -39,9 +49,16 @@ class SystemTrackController extends AbstractController
             return $this->render("bundles/TwigBundle/Exception/error404.html.twig");
         }
         $OrderReposotory =  $this->getDoctrine()->getRepository(Order::class);
+
+        /** @var Order $Order */
+
         $Order =  $OrderReposotory->find($request->request->get('id_order'));
         $Order->setSystemNumInUsa($request->request->get('systemNumInUsa'));
         $entityManager = $this->getDoctrine()->getManager();
+        $companyTmp=$request->request->get('company',false);
+
+        $company = ($companyTmp && in_array(OrdersAdmin::CARRIER_CODES))?$companyTmp:'usps';
+        $Order->setCompanySendInUsa($company);
         $entityManager->persist($Order);
         $entityManager->flush();
         dd('success');
